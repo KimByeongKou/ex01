835c1efd1a39e1fb0299daefd90aef06420b7399
Kim ByeongKou <anapple63@gmail.com>
2016-03-27 01:11:06 +0900

    commit

--- commit.sh
+++ commit.sh
@@ -2,5 +2,6 @@
 #git remote add origin https://github.com/KimByeongKou/ex01.git
 #git pull origin master
 git add .
+git remote add origin https://github.com/KimByeongKou/ex01.git
 git commit -m "commit"
 git push -u origin master
